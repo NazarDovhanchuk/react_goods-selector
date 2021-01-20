bbe68f7104f6ff11d387d25d2d91fcc04f7f4060
Nazar <nazardovhanchuk@gmail.com>
2021-01-20 10:39:40 +0200

    Updates

---
+++ precache-manifest.1de767ee72c879852eb014713f1a736b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e391ed7270264015431d5abdd0e25969",
+    "url": "./index.html"
+  },
+  {
+    "revision": "115795d92b4b1b3334af",
+    "url": "./static/css/main.464c7c87.chunk.css"
+  },
+  {
+    "revision": "b5ebe3227a36fe6a4ae7",
+    "url": "./static/js/2.c3d77166.chunk.js"
+  },
+  {
+    "revision": "8a137f586fcebb1a370343eb47b7677c",
+    "url": "./static/js/2.c3d77166.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "115795d92b4b1b3334af",
+    "url": "./static/js/main.035b0037.chunk.js"
+  },
+  {
+    "revision": "f861d31e3669d4c8fef0",
+    "url": "./static/js/runtime-main.c970f6dd.js"
+  }
+]);
